ae7dc3999cb5612e9e0b393c1371dfbfee2f0d25
Logvina <irinlogvina9@gmail.com>
2021-11-17 23:19:25 +0200

    Update currentUser.js

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -1,19 +1,24 @@
-const currentUser = async (req, res, next) => {
-    try {
-      // const id = req.user._id;
-      const {_id:id, name, email, subscription } = req.user;
-      return res.status(200).json({
-        status: "success",
-        user: {
-          id,
-          name,
-          email,
-          subscription,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
+const { Unauthorized } = require("http-errors");
+const { User } = require("../../schema/user");
+
+const currentUser = async (req, res) => {
+  const { token } = req.user;
+  const user = await User.findOne({ token });
+
+  if (!user) {
+    throw new Unauthorized("Not authorized");
+  }
+  const { email, subscription } = user;
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      email,
+      subscription,
+    },
+  });
+};
+
 
   module.exports = currentUser;
