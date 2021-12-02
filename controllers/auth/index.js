a6cac0f6d12754eb5e868b5e4b73fd8075abbc48
Logvina <irinlogvina9@gmail.com>
2021-12-02 13:50:39 +0200

    2

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,11 +3,14 @@ const login = require("./login");
 const logout = require("./logout");
 const currentUser = require("./currentUser");
 const updateIMG = require("./updateIMG");
+const verify = require("./verify");
 
 module.exports = {
   register,
   login,
   logout,
   currentUser,
-  updateIMG
+  updateIMG,
+  verify,
+
 };
